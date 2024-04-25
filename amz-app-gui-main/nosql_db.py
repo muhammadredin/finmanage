346f5aaedd3fff2a47be3b0533998b6c17b44bb8
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 09:58:57 +0700

    Add files via upload

---
+++ amz-app-gui-main/nosql_db.py
@@ -0,0 +1,25 @@
+import boto3
+from decimal import Decimal
+
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+
+# Define table name
+table_name = 'finapp_users'
+
+# Get reference to the DynamoDB table
+table = dynamodb.Table(table_name)
+
+# Example: Inserting a new item into the table
+new_item = {
+    'username': 'john_doe',  # This should be the username value
+    'fullname': 'John Doe',
+    'birthdate': '1990-01-01',
+    'password': 'password123',
+    'account_balance': Decimal('1000.00')
+}
+
+table.put_item(Item=new_item)
+
+print("Item updated successfully.")
