03173d07b33abfea2bfadc7c56ba83c17356f34c
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 11:18:18 +0700

    Update nosql_db.py

--- amz-app-gui-main/nosql_db.py
+++ amz-app-gui-main/nosql_db.py
@@ -1,25 +1,54 @@
 import boto3
+import hashlib
+import streamlit as st
+
+from boto3.dynamodb.conditions import Key
 from decimal import Decimal
 
 
-# Initialize DynamoDB client
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+def connect_db():
+    # Initialize DynamoDB client
+    dynamodb = boto3.resource('dynamodb')
 
-# Define table name
-table_name = 'finapp_users'
+    # Define table name
+    table_name = 'finapp_users'
 
-# Get reference to the DynamoDB table
-table = dynamodb.Table(table_name)
+    # Get reference to the DynamoDB table
+    table = dynamodb.Table(table_name)
+    
+    return table
 
-# Example: Inserting a new item into the table
-new_item = {
-    'username': 'john_doe',  # This should be the username value
-    'fullname': 'John Doe',
-    'birthdate': '1990-01-01',
-    'password': 'password123',
-    'account_balance': Decimal('1000.00')
-}
-
-table.put_item(Item=new_item)
-
-print("Item updated successfully.")
+def add_users(table, username, fullname, birthdate, password, balance=0):
+    # Check if username already exists
+    response = table.query(
+        KeyConditionExpression=Key('username').eq(username)
+    )
+    if response.get('Items'):
+        st.error("Username already exists. Please choose a different username.")
+        return False
+    else:
+        # Hash the password
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        # Store user information in DynamoDB
+        table.put_item(Item={
+            'username': username,  # This should be the username value
+            'fullname': fullname,
+            'birthdate': birthdate.isoformat(),
+            'password': password_hash,
+            'account_balance': balance
+        })
+        
+        st.success("Registration successful. You can now log in.")
+        return True
+    
+def login_user(table, username, password):
+    # Retrieve user information from DynamoDB
+    response = table.get_item(Key={'username': username})
+    if 'Item' in response:
+        # Verify password
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+        if response['Item']['password'] == password_hash:
+            st.success("Login successful.")
+            return response['Item']
+    st.error("Invalid username or password.")
+    return False
