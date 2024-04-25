cdcd9c50876e18b72901c3bc3bfcab924018cb16
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 11:57:53 +0700

    Update input_spend.py

--- amz-app-gui-main/stpages/input_spend.py
+++ amz-app-gui-main/stpages/input_spend.py
@@ -3,10 +3,16 @@ import streamlit as st
 
 from datetime import date
 
+def get_user_data():
+    return st.session_state.user_data
+
 def run():
     conn = finance_db.connect_db()
 
     st.title("Add Income")
+
+    user_data = get_user_data()
+    
     date_income = st.date_input("Income Date", min_value=date(1970, 1, 1))
         
     income = st.number_input("Money Received", min_value=0, step=100)
@@ -16,10 +22,11 @@ def run():
     income_category = st.selectbox("Income Category", income_options)
 
     income_note = st.text_input("Note", key="income_note")
+
     
     if st.button("Submit", key="income"):
-        sql = "insert into incomes(income_date, income, category, note) VALUES (%s, %s, %s, %s)"
-        data = (date_income, income, income_category, income_note)
+        sql = "insert into incomes(username, income_date, income, category, note) VALUES (%s, %s, %s, %s, %s)"
+        data = (user_data['username'], date_income, income, income_category, income_note)
         finance_db.create_record(conn, sql, data)
         st.success("Record created successfully.")
 
