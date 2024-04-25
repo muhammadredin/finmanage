7e2894ab24390359a8da842451b308025efc5fcd
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 11:19:16 +0700

    Update main.py

--- amz-app-gui-main/main.py
+++ amz-app-gui-main/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from stpages import dashboard, signup, login, personalization, expense_page, chatbot, spend_track, input_spend
+from stpages import dashboard, signup, login, personalization, expense_page, chatbot, spend_track, input_spend, user_dashboard
 import tools
 import remove_footer
 
@@ -26,14 +26,17 @@ if __name__ == "__main__":
         spend_track.run()
     elif page == 'input_spend':
         input_spend.run()
+    elif page == 'user_dashboard':
+        user_dashboard.run()
 
     if page != None:
         st.sidebar.button("Introduction", on_click=tools.change_page('dashboard'))
+        st.sidebar.button("User Dashboard", on_click=tools.change_page('user_dashboard'))
         #st.sidebar.markdown("Authentication")
         st.sidebar.button("Login Form", on_click=tools.change_page('login'))
         st.sidebar.button("Sign Up Form", on_click=tools.change_page('signup'))
         st.sidebar.button("Personalization", on_click=tools.change_page('personalization'))
         st.sidebar.button("Expense Allocation", on_click=tools.change_page("expense_page"))
         st.sidebar.button("Spend Track", on_click=tools.change_page("spend_track"))
-        st.sidebar.button("Add Outcomes", on_click=tools.change_page("input_spend"))
+        st.sidebar.button("Add Income/Outcome", on_click=tools.change_page("input_spend"))
     remove_footer.run()
