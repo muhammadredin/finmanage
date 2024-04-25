bf22e4903b2151986bd4fcb91915fab6f6e0b6b4
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 18:59:46 +0700

    Update input_spend.py

--- amz-app-gui-main/stpages/input_spend.py
+++ amz-app-gui-main/stpages/input_spend.py
@@ -1,4 +1,5 @@
 import finance_db
+import nosql_db
 import streamlit as st
 
 from datetime import date
@@ -8,6 +9,7 @@ def get_user_data():
 
 def run():
     conn = finance_db.connect_db()
+    nosql_conn = nosql_db.connect_db()
 
     st.title("Add Income")
 
@@ -28,6 +30,13 @@ def run():
         sql = "insert into incomes(username, income_date, income, category, note) VALUES (%s, %s, %s, %s, %s)"
         data = (user_data['username'], date_received, money_received, category, note)
         finance_db.create_record(conn, sql, data)
+        response = nosql_conn.get_item(Key={'username': user_data['username']})
+
+        if item in response:
+            item = response['Item']
+            item['account_balance'] += money_received
+            table.put_item(Item=item)
+        
         st.success("Record created successfully.")
 
     st.title("Add Outcome")
@@ -45,4 +54,10 @@ def run():
         sql = "insert into spending(username, date_spend, money_spend, category, note) VALUES (%s, %s, %s, %s, %s)"
         data = (user_data['username'], date_spend, outcome, outcome_category, spend_note)
         finance_db.create_record(conn, sql, data)
+
+        if item in response:
+            item = response['Item']
+            item['account_balance'] -= outcome
+            table.put_item(Item=item)
+        
         st.success("Record created successfully.")
