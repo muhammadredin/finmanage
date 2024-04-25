6e5eb13128e3a1ae765f302b0e5f65106062cde2
Muhammad Redin Birezqic <115381364+muhammadredin@users.noreply.github.com>
2024-04-25 11:21:50 +0700

    Update login.py

--- amz-app-gui-main/stpages/login.py
+++ amz-app-gui-main/stpages/login.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import tools
 import time
+import nosql_db
+
+def set_session_state(user_data):
+    st.session_state.user_data = user_data
 
 def run():
 	# Add the Login title
 	st.title("Login")
 	st.markdown("Login to access your personalized dashboard, where you'll discover a wealth of tools and resources designed to supercharge your financial decision-making.")
 
+	table = nosql_db.connect_db()
 	# Add the username and password input fields
 	username = st.text_input("Username")
 	password = st.text_input("Password", type="password")
@@ -16,13 +21,7 @@ def run():
 	col2.button("Doesn't have an account?", on_click=tools.change_page('signup'))
 
 	def reg():
-		accounts = tools.Storage('accounts.json')
-		# Check if the username and password are valid
-		if accounts.get(username) == None:
-			st.error("Invalid username or password. Please try again.")
-		elif accounts.get(username).get('password') != password:
-			st.error("Invalid username or password. Please try again.")
-		else:
-			tools.change_page('expense_page')()
+		data = nosql_db.login_user(table, username, password)
+		set_session_state(data)
 	# Add the login button
 	col1.button("Login", on_click=reg)
